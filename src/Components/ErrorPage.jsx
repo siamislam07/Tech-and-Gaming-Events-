155b960b35673bb788f44c71d5dc9dcf3fab1315
siamislam07 <siamislam1970@gmail.com>
2023-10-09 22:53:24 +0600

    some bug fixed

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
     return (
         <div className="text-center w-full h-[80vh]">
             <h1 className="text-5xl">Oops!</h1>
             <p className="text-3xl">Not Found Any Pages</p>
+            <Link><button className="btn ">Home</button></Link>
         </div>
     );
 };
